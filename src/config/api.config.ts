ed28658e4064edd5c1982f614d2bcb147c03eb37
Paulo Assis <phanet@gmail.com>
2020-05-06 23:31:43 -0300

    Mostrando as Categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
+    baseUrl: "http://localhost:8080",
+    BucketBaseUrl: "https://curso-spring-ionic-phanet.s3-sa-east-1.amazonaws.com"
 }
--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaService } from '../../services/domain/categoria.service';
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 /**
  * Generated class for the CategoriasPage page.
@@ -16,6 +18,10 @@ import { CategoriaService } from '../../services/domain/categoria.service';
 })
 export class CategoriasPage {
 
+  items: CategoriaDTO[];  //essa é a lista que será exporta no meu controlador para que o html possa ler os dados
+
+  bucketUrl: String = API_CONFIG.BucketBaseUrl;
+
   constructor(
               public navCtrl: NavController, 
               public navParams: NavParams,
@@ -25,7 +31,10 @@ export class CategoriasPage {
   ionViewDidLoad() {  //--significa quando a página terminar de ser carregada será executada o que estiver aqui dentro
     this.categoriaService.findAll()  //-- essa é uma chamada assincrona e tem que se inscrever para fazer alguma coisa quando a resposta chegar tem que usar o .subscribe
       .subscribe(response => { //foi utilizada uma função anonima chamada de "arrow function") 
-      console.log(response) //-- dentro dele, terá que usar uma função "callback" para ser executada quando a resposta chegar    
+        this.items = response; // Desta forma lhe dá condição de pegar o categoria.html e fazer ele ler a lista de itens "CategoriaDTO[]"
+      
+      
+      //console.log(response) //-- dentro dele, terá que usar uma função "callback" para ser executada quando a resposta chegar    
     }, //caso a requisição der erro 
     error =>{
       console.log(error)

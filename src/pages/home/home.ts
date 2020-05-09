fa102f8544864c01a4da01f9ea979edf802e9fb1
Paulo Assis <phanet@gmail.com>
2020-05-08 23:05:54 -0300

    Obtendo dados do formulário de login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage, MenuController } from 'ionic-angular';
+import { CredenciaisDTO } from '../../models/credenciais.dto';
+import { hydrateSegmentsWithNav } from 'ionic-angular/umd/navigation/url-serializer';
 
 @IonicPage()
 @Component({
@@ -9,6 +11,11 @@ import { NavController, IonicPage, MenuController } from 'ionic-angular';
 export class HomePage {
   constructor(public navCtrl: NavController, public menu: MenuController) {
   }
+
+  creds : CredenciaisDTO = {
+    email:"",
+    senha:""  
+  };
 
   ionViewWillEnter() {
     this.menu.swipeEnable(false);
@@ -20,6 +27,8 @@ export class HomePage {
 
   login(){
     //this.navCtrl.push('CategoriasPage');                             //--push é um metodo de empilamento de página que ao chamar uma pagina ela fica sobre a outra.
+    
+    console.log(this.creds);
     this.navCtrl.setRoot('CategoriasPage');                            //--  SetRoot navegação sem empilhar 
   }
 }
